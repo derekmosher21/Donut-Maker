b2703c78d286082858873058a5597ed0d584e0cf
Derek Mosher <derekmosher21@gmail.com>
2020-08-12 09:09:57 -0400

    added function for checkDonutFundsToBuyAutoClicker

--- src/js/app.js
+++ src/js/app.js
@@ -1,18 +1,29 @@
 const donutClicker = new DonutMaker();
 setupDonutButton();
 updateDonutCount();
+checkDonutFundsToBuyAutoClicker();
 
 function setupDonutButton() {
   const donutButton = document.querySelector(".donut");
   donutButton.addEventListener("click", () => {
     donutClicker.clickDonut();
     updateDonutCount();
+    checkDonutFundsToBuyAutoClicker();
   });
 }
 
 function updateDonutCount() {
   const donutCountSpan = document.querySelector(".donut-count");
   donutCountSpan.innerText = donutClicker.getClickCount();
+}
+
+function checkDonutFundsToBuyAutoClicker() {
+  const autoClickerButton = document.querySelector(".buyAutoClickerButton");
+  if(donutClicker.hasInsufficientDonutsToBuyAutoClicker()) {
+    autoClickerButton.disabled = true;
+  } else {
+    autoClickerButton.disabled = false;
+  }
 }
 
 // buyAutoClickerButton.addEventListener("click", () => {
